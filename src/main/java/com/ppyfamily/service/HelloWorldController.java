325b740462d89a9a4a82a07692eb8f5ff9b96386
Poom1997 <Poom1997@users.noreply.github.com>
2019-08-17 00:14:11 +0700

    create hello world controller

---
+++ src/main/java/com/ppyfamily/service/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.ppyfamily.service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@GetMapping("hello")
+	public String helloWorld(){
+		return "Hello World!";
+	}
+}
