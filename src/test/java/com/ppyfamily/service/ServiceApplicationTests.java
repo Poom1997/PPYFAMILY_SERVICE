1342e27260c5cb88596b13067084f2bb65417f5f
Poom Wongsrisakul <Poom1997@users.noreply.github.com>
2020-04-20 16:14:16 +0700

    Update workflow fix

--- src/test/java/com/ppyfamily/service/ServiceApplicationTests.java
+++ src/test/java/com/ppyfamily/service/ServiceApplicationTests.java
@@ -3,14 +3,17 @@ package com.ppyfamily.service;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("local")
 public class ServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
 	}
 
 }
